dbde2f4881529a8d73d4c61af67868c6d25a7839
Stefan Fiel <stefan@nomacs.org>
2016-07-12 08:15:38 +0200

    update "working" (untested) before loging utility

--- DependencyCollector.py
+++ DependencyCollector.py
@@ -1,34 +1,62 @@
 #!/usr/local/bin/python
 
+OUTPUT_NAME = "DependencyCollector"
+ERROR_MSG = OUTPUT_NAME + ": ERROR: "
+WARNING_MSG = OUTPUT_NAME + ": WARNING: "
+
 def parse_config_file(configfile, configuration):
-    mapping = ""
-    debug_paths = ""
-    optimized_paths = ""
-    config_create = ""
-    config_paths = ""
+    import configparser
+    config = configparser.ConfigParser();
 
-    for line in open(configfile, 'r'):
-        if "DEPENDENCY_COLLECTOR_MAPPING_" + configuration in line:
-            print("configuration mapping found")
-            mapping = "PLEASE SET ME CORRECTLY"
-        if "DEPENDENCY_COLLECTOR_CREATE_" + configuration in line:
-            print("configuration create found")
-            config_create = "PLEASE SET ME CORRECTLY"
-        if "DEPENDENCY_COLLECTOR_PATHS_OPTIMIZED" in line:
-            print("optimized paths found")
-            optimized_paths = "PLEASE SET ME CORRECTLY"
-        if "DEPENDENCY_COLLECTOR_PATHS_DEBUG" in line:
-            print("debug paths found")
-            debug_paths = "PLEASE SET ME CORRECTLY"
-
-    if mapping.lower() == "debug":
-        config_paths = debug_paths
-    elif mapping.lower() == "release":
-        config_paths = optimized_paths
-    else:
-        print("DependencyCollector: error: mapping of configuration not found in config file")
+    config.read(configfile)
+    mapping = config['DependencyCollector']['MAPPING_'+configuration]
+    if not (mapping.lower() == "debug" or mapping.lower() == "release"):
+        print(ERROR_MSG + " mapping of configuration not found or incorrect in config file")
         exit()
-    return(config_create,config_paths)
+
+    create = config.getboolean('DependencyCollector','CREATE_'+configuration)
+    paths_string = config['DependencyCollector']['PATHS_'+mapping.upper()]
+    paths = paths_string.split(';')
+    blacklist = "";
+    if config.has_option('DependencyCollector','BLACKLIST'):
+        blacklist = config['DependencyCollector']['BLACKLIST']
+        blacklist = blacklist.split(';')
+    return(create,paths,blacklist)
+
+def update_mode(infile, config_paths, config_blacklist):
+    import glob
+    import ntpath
+
+    print("update mode")
+    dir = os.path.dirname(os.path.realpath(infile))
+    existing_dlls = glob.glob(dir+"/*.ini")
+
+    print("existing dlls:" + str(existing_dlls))
+    for dll in existing_dlls:
+        dll_name = ntpath.basename(dll)
+        if dll_name not in config_blacklist:
+            (newest_dll, mod_date) = search_for_newest_file(dll_name, config_paths)
+            if mod_date < os.path.getmtime(dll):
+                print("COPY FILE HERE!!")
+    return
+
+def create_mode(infile, config_paths, config_blacklist):
+    print("create mode")
+    return
+
+def search_for_newest_file(file, paths):
+    newest_file = ""
+    mod_date = ""
+    for p in paths:
+        print(p + "/" + file)
+        if os.path.isfile(p+ "/" + file):
+            if mod_date == "" or os.path.getmtime(p+ "/" + file) < mod_date:
+                mod_date = os.path.getmtime(p+ "/" + file)
+                newest_file = p+ "/" + file
+    if newest_file == "":
+        print(WARNING_MSG + ": no dll found for " + file)
+
+    return(newest_file, mod_date)
 
 if __name__ == "__main__":
     import argparse
@@ -48,11 +76,19 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     if not os.path.isfile(args.infile):
-        print("DependencyCollector: error: input file does not exist")
+        print(ERROR_MSG + "input file does not exist")
         exit()
     if not os.path.isfile(args.configfile):
-        print("DependencyCollector: error: config file does not exist")
+        print(ERROR_MSG + "config file does not exist")
         exit()
 
-    (config_create, config_paths) = parse_config_file(args.configfile, args.configuration)
+    (config_create, config_paths, config_blacklist) = parse_config_file(args.configfile, args.configuration)
 
+    print("config_create:" + str(config_create))
+    if config_create == True:
+        create_mode(args.infile, config_paths, config_blacklist)
+    elif config_create == False:
+        update_mode(args.infile, config_paths, config_blacklist)
+    else:
+        print(ERROR_MSG + "create mode unkown")
+        exit()
